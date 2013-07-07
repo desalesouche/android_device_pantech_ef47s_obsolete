cb9208a952383bac7afb65774b7332cf75a0f59f
AlexBokhankovich <alex.bokhankovich@gmail.com>
2013-07-07 16:24:05 +0400

    Move ramdisk files to device config

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -111,28 +111,3 @@ TARGET_PROVIDES_POWERHAL := true
 ENABLE_WEBGL := true
 TARGET_FORCE_CPU_UPLOAD := true
 
-# Boot ramdisk setup
-PRODUCT_COPY_FILES += \
-  device/pantech/ef47s/ramdisk/fstab.qcom:root/fstab.qcom \
-  device/pantech/ef47s/ramdisk/init.goldfish.rc:root/init.goldfish.rc \
-  device/pantech/ef47s/ramdisk/initlogo.rle:root/initlogo.rle \
-  device/pantech/ef47s/ramdisk/init.pantech.usb.rc:root/init.pantech.usb.rc \
-  device/pantech/ef47s/ramdisk/init.pantech.usb.sh:root/init.pantech.usb.sh \
-  device/pantech/ef47s/ramdisk/init.qcom.class_core.sh:root/init.qcom.class_core.sh \
-  device/pantech/ef47s/ramdisk/init.qcom.class_main.sh:root/init.qcom.class_main.sh \
-  device/pantech/ef47s/ramdisk/init.qcom.rc:root/init.qcom.rc \
-  device/pantech/ef47s/ramdisk/init.qcom.sh:root/init.qcom.sh \
-  device/pantech/ef47s/ramdisk/init.qcom.syspart_fixup.sh:root/init.qcom.syspart_fixup.sh \
-  device/pantech/ef47s/ramdisk/init.qcom.usb.rc:root/init.qcom.usb.rc \
-  device/pantech/ef47s/ramdisk/init.qcom.usb.sh:root/init.qcom.usb.sh \
-  device/pantech/ef47s/ramdisk/init.target.rc:root/init.target.rc \
-  device/pantech/ef47s/ramdisk/init.trace.rc:root/init.trace.rc \
-  device/pantech/ef47s/ramdisk/init.usb.rc:root/init.usb.rc \
-  device/pantech/ef47s/ramdisk/poweroff_in.rle:root/poweroff_in.rle \
-  device/pantech/ef47s/ramdisk/resetlogo.rle:root/resetlogo.rle \
-  device/pantech/ef47s/ramdisk/ueventd.goldfish.rc:root/ueventd.goldfish.rc \
-  device/pantech/ef47s/ramdisk/ueventd.qcom.rc:root/ueventd.qcom.rc \
-  device/pantech/ef47s/ramdisk/ueventd.rc:root/ueventd.rc
-#  device/pantech/ef47s/ramdisk/charger:root/charger
-#  device/pantech/ef47s/ramdisk/init.rc:root/init.rc \
-#  device/pantech/ef47s/ramdisk/default.prop:root/default.prop \
