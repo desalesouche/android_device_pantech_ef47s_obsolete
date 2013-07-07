dea1c4fcf97b0decfe24ad9487c8bebe8f2f1ba7
AlexBokhankovich <alex.bokhankovich@gmail.com>
2013-07-07 20:08:27 +0400

    device_ef47s.mk preparation for build

--- device_ef47s.mk
+++ device_ef47s.mk
@@ -9,7 +9,7 @@ DEVICE_PACKAGE_OVERLAYS += device/pantech/ef47s/overlay
 
 LOCAL_PATH := device/pantech/ef47s
 ifeq ($(TARGET_PREBUILT_KERNEL),)
-	LOCAL_KERNEL := $(LOCAL_PATH)/kernel
+	LOCAL_KERNEL := device/pantech/ef47s/kernel
 else
 	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
 endif
@@ -22,12 +22,25 @@ $(call inherit-product, build/target/product/full.mk)
 # Set build date
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
+# Audio config
+PRODUCT_COPY_FILES += \
+    device/pantech/ef47s/configs/audio_policy.conf:system/etc/audio_policy.conf
+
+# Thermal config
+PRODUCT_COPY_FILES += \
+    device/pantech/ef47s/configs/thermald.conf:system/etc/thermald.conf
+
+# GPS config
+PRODUCT_COPY_FILES += \
+    device/pantech/ef47s/gps/gps.conf:system/etc/gps.conf
+
+
 # Discard inherited values and use our own instead.
 PRODUCT_NAME := ef47s
 PRODUCT_DEVICE := ef47s
 PRODUCT_BRAND := SKY
-PRODUCT_MODEL := Vega Racer 2
-PRODUCT_MANUFACTURER := Pantech
+PRODUCT_MODEL := IM-A830S
+PRODUCT_MANUFACTURER := PANTECH
 
 # Boot ramdisk setup
 PRODUCT_COPY_FILES += \
@@ -55,12 +68,173 @@ PRODUCT_COPY_FILES += \
 #  device/pantech/ef47s/ramdisk/init.rc:root/init.rc \
 #  device/pantech/ef47s/ramdisk/default.prop:root/default.prop \
 
+
+# WPA supplicant config
+PRODUCT_COPY_FILES += \
+    device/pantech/ef47s/configs/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf
+
+# Media config
+PRODUCT_COPY_FILES += \
+    device/pantech/ef47s/configs/audio_policy.conf:system/etc/audio_policy.conf \
+    device/pantech/ef47s/configs/media_profiles.xml:system/etc/media_profiles.xml \
+    device/pantech/ef47s/configs/media_codecs.xml:system/etc/media_codecs.xml
+
+# QC thermald config
+PRODUCT_COPY_FILES +=device/pantech/ef47s/configs/thermald.conf:system/etc/thermald.conf
+
+# vold config
+PRODUCT_COPY_FILES += \
+    device/pantech/ef47s/configs/vold.fstab:system/etc/vold.fstab
+
+# Sound configs
+PRODUCT_COPY_FILES += \
+    device/pantech/ef47s/dsp/snd_soc_msm/snd_soc_msm_2x:/system/etc/snd_soc_msm/snd_soc_msm_2x
+
+# Keylayouts and Keychars
+PRODUCT_COPY_FILES += \
+    device/pantech/ef47s/keylayout/atmel_mxt_ts.kl:system/usr/keylayout/AVRCP.kl \
+    device/pantech/ef47s/keylayout/keypad_8960.kl:system/usr/keylayout/h2w_headset.kl \
+    device/pantech/ef47s/keylayout/Button_Jack.kl:system/usr/keylayout/msm8960-snd-card_Button_Jack.kl \
+    device/pantech/ef47s/keylayout/qwerty.kl:system/usr/keylayout/qwerty.kl \
+    device/pantech/ef47s/keylayout/qt602240_ts_input_ef47s.kl:system/usr/keylayout/synaptics-rmi-touchscreen.kl
+	
+# Input device config
+PRODUCT_COPY_FILES += \
+    device/pantech/ef47s/idc/qwerty2.idc:system/usr/idc/qwerty2.idc \
+    device/pantech/ef47s/idc/qwerty.idc:system/usr/idc/qwerty.idc \
+
+# NFC
+PRODUCT_PACKAGES += \
+    libnfc \
+    libnfc_ndef \
+    libnfc_jni \
+    Nfc \
+    Tag \
+    com.android.nfc_extras
+	
+# Torch
+PRODUCT_PACKAGES += \
+    Torch
+
+# Graphics
+PRODUCT_PACKAGES += \
+    copybit.msm8960 \
+    gralloc.msm8960 \
+    hwcomposer.msm8960 \
+    libgenlock \
+    liboverlay
+
+# Audio
+PRODUCT_PACKAGES += \
+    alsa.msm8960 \
+    audio.a2dp.default \
+    audio_policy.msm8960 \
+    audio.primary.msm8960 \
+    audio.r_submix.default \
+    audio.usb.default \
+    libaudio-resampler \
+    tinymix
+# Lights
+PRODUCT_PACKAGES += \
+    lights.msm8960
+
+# OMX
+PRODUCT_PACKAGES += \
+    libc2dcolorconvert \
+    libdivxdrmdecrypt \
+    libOmxCore \
+    libOmxVdec \
+    libOmxVenc \
+    libOmxAacEnc \
+    libOmxAmrEnc \
+    libOmxEvrcEnc \
+    libOmxQcelp13Enc \
+    libstagefrighthw
+
+# Power
+PRODUCT_PACKAGES += \
+    power.msm8960
+
+# HDMI
+PRODUCT_PACKAGES += \
+    hdmid
+
+# QCOM rngd
+PRODUCT_PACKAGES += \
+    qrngd
+
+# USB
+PRODUCT_PACKAGES += \
+    com.android.future.usb.accessory
+
+# Filesystem management tools
+PRODUCT_PACKAGES += \
+    make_ext4fs \
+    setup_fs
+
+# overlays
+DEVICE_PACKAGE_OVERLAYS += device/pantech/ef47s/overlay
+
+# Qualcomm scripts
+PRODUCT_COPY_FILES += \
+    device/pantech/ef47s/configs/init.qcom.bt.sh:/system/etc/init.qcom.bt.sh \
+    device/pantech/ef47s/configs/init.qcom.fm.sh:/system/etc/init.qcom.fm.sh \
+    device/pantech/ef47s/configs/init.qcom.post_boot.sh:/system/etc/init.qcom.post_boot.sh \
+    device/pantech/ef47s/configs/init.qcom.sdio.sh:/system/etc/init.qcom.sdio.sh \
+    device/pantech/ef47s/configs/init.qcom.wifi.sh:/system/etc/init.qcom.wifi.sh
+	
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+    frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardwardware.sensor.gyroscope.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+    frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.compass.xml \
+    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/base/nfc-extras/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
+    packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml
+		
 # Extra properties
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.setupwizard.enable_bypass=1 \
     dalvik.vm.lockprof.threshold=500 \
     ro.com.google.locationfeatures=1 \
-    dalvik.vm.dexopt-flags=m=y
+    dalvik.vm.dexopt-flags=m=y \
+    com.qc.hardware=true \
+    debug.composition.type=dyn \
+    debug.egl.hw=1 \
+    debug.mdpcomp.logs=0 \
+    debug.sf.hw=1 \
+    dev.pm.dyn_samplingrate=1 \
+    lpa.decode=true \
+    persist.audio.fluence.mode=endfire \
+    persist.audio.vr.enable=false \
+    persist.audio.handset.mic=digital \
+    persist.audio.speaker.location=high \
+    persist.hwc.mdpcomp.enable=true \
+    persist.thermal.monitor=true \
+    ro.opengles.version=131072 \
+    ro.qc.sdk.audio.fluencetype=fluence \
+    ro.use_data_netmgrd=true \
+    persist.gps.qmienabled=true \
+    ro.baseband.arch=msm \
+    ro.product.wireless=WCN3660 \
+    ro.qualcomm.bt.hci_transport=smd \
+    ro.telephony.ril.v3=skipradiooff
+    wifi.interface=wlan0
+
+# We have enough space to hold precise GC data
+PRODUCT_TAGS += dalvik.gc.type-precise
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
